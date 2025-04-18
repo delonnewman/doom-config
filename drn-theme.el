c063b4283c76b170f6909e996fed2e15628042dc
Delon Newman <contact@delonnewman.name>
2025-04-18 11:01:17 -0400

    Improve theme loading code

--- drn-theme.el
+++ drn-theme.el
@@ -10,9 +10,27 @@
 (defvar drn/light-theme 'modus-operandi
   "Sets the theme to use when the UI is in light mode.")
 
+;; Flagrantly stolen https://emacsredux.com/blog/2025/02/03/clean-unloading-of-emacs-themes/
+(defun drn/disable-all-active-themes ()
+  "Disable all currently active themes."
+  (interactive)
+  (dolist (theme custom-enabled-themes)
+    (disable-theme theme)))
+
 (defun drn/load-theme (theme)
+  (drn/disable-all-active-themes)
   (setq doom-theme theme)
   (load-theme theme t))
+
+(defun drn/load-dark-theme ()
+  "Load dark theme configured with drn/dark-theme."
+  (interactive)
+  (drn/load-theme drn/dark-theme))
+
+(defun drn/load-light-theme ()
+  "Load dark theme configured with drn/light-theme."
+  (interactive)
+  (drn/load-theme drn/light-theme))
 
 (drn/load-theme drn/light-theme)
 
