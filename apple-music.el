28454dc8a9157a6b286601c3b2053bcd278794a4
Delon Newman <contact@delonnewman.name>
2026-02-19 09:13:21 -0500

    Add reminders and podcasts functions

--- apple-music.el
+++ apple-music.el
@@ -8,8 +8,6 @@
 
 ;;; Code:
 
-(require 'applescript)
-
 (defun apple-music-open ()
   "Open Apple Music."
   (interactive)
@@ -31,5 +29,39 @@ If SHUFFLE is non-nil the music will play in suffle mode."
   "Pause music playing in Apple Music."
   (interactive)
   (applescript (tell (application "Music") (activate) (pause))))
+
+
+(defun apple-reminders-open ()
+  "Open Apple Reminders."
+  (interactive)
+  (applescript
+   (tell (application "Reminders")
+     (activate))))
+
+(defun jw-library-open ()
+  "Open JW Library."
+  (interactive)
+  (applescript (tell (application "JW Library") (activate))))
+
+(defun apple-podcasts-open ()
+  "Open Apple Podcasts."
+  (interactive)
+  (applescript (tell (application "Podcasts") (activate))))
+
+;; tell application "System Events"
+;;   tell process "JW Library"
+;;     set frontmost to true
+;;     click menu item "Move to Built-in Retina Display" of menu "Window" of menu bar 1
+;;     click menu item "Enter Full Screen" of menu "View" of menu bar 1
+;;   end tell
+;; end tell
+
+;; (defun move-application-to-extra-display (application)
+;;   (applescript-eval
+;;    `(tell (application "System Events")
+;;      (tell (process ,application)
+;;       (set frontmost t)
+;;       (click-menu-item "Move to Built-in Retina Display" :of-menu "Window" :of-menu-bar 1))))
+;;   )
 
 (provide 'apple-music)
