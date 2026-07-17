cb80e41934e0d0033cb8e8356379150d9b87b7a5
Delon Newman <contact@delonnewman.name>
2026-07-17 15:41:01 -0400

    Improve Perl config

--- drn-perl.el
+++ drn-perl.el
@@ -1,33 +1,37 @@
 ;;; perl.el -*- lexical-binding: t; -*-
 ;;; see https://www.emacswiki.org/emacs/CPerlMode
 
-(require 'flycheck)
-(require 'lsp-mode)
-(require 'web-mode)
-(require 'cperl-mode)
+(def-project-mode! +web-mojo-mode
+  :modes '(web-mode cperl-mode)
+  :files (or "cpanfile" "lib/" "templates/")
+  :match ".*\\.pl"
+  :on-enter
+  (when (eq major-mode 'web-mode)
+    (web-mode-set-engine "mojolicious")))
 
-(fset 'perl-mode 'cperl-mode)
+(after! cperl-mode
+  (fset 'perl-mode 'cperl-mode)
+  (setq cperl-indent-level 2)
+  (setq cperl-indent-parens-as-block t)
+  (setq cperl-close-paren-offset (- cperl-indent-level))
+  (add-to-list 'auto-mode-alist '("\\.t$" . cperl-mode))
+  (add-to-list 'auto-mode-alist '("cpanfile" . cperl-mode)))
 
-(setq cperl-indent-level 2)
+(after! flycheck
+  (add-hook 'cperl-mode-hook 'flycheck-mode)
+  (setq flycheck-check-syntax-automatically '(idle-change save mode-enabled))
+  (setq flycheck-display-errors-delay 0.3))
 
-(setq cperl-indent-parens-as-block t)
-(setq cperl-close-paren-offset (- cperl-indent-level))
-
-(setq flycheck-check-syntax-automatically '(idle-change save mode-enabled))
-(setq flycheck-display-errors-delay 0.3)
-
-(add-hook 'cperl-mode-hook #'lsp) ;; TODO: figure out how to configure PERL5LIB
-(add-hook 'cperl-mode-hook 'flycheck-mode)
-(add-to-list 'auto-mode-alist '("\\.t$" . cperl-mode))
-(add-to-list 'auto-mode-alist '("cpanfile" . cperl-mode))
+(after! lsp-mode
+  (add-to-list 'lsp-language-id-configuration '(".*\\.ep$" . "html"))
+  (add-hook 'cperl-mode-hook 'lsp-mode)) ;; TODO: figure out how to configure PERL5LIB
 
 ;; Mojolicious
-(add-to-list 'auto-mode-alist '("\\.html.ep" . web-mode))
-(add-to-list 'web-mode-engines-alist '("mojolicious" . "\\.ep"))
+(after! web-mode
+  (add-to-list 'auto-mode-alist '("\\.html.ep" . web-mode))
+  (add-to-list 'web-mode-engines-alist '("mojolicious" . "\\.ep")))
 
-(set-docsets! 'cperl-mode "Perl" "HTML" "CSS" "HTTP" "Bootstrap_5" "C" "Sass")
-
-(add-to-list 'lsp-language-id-configuration '(".*\\.ep$" . "perl"))
+;; (set-docsets! 'cperl-mode "Perl" "HTML" "CSS" "HTTP" "Bootstrap_5" "C" "Sass")
 
 ;; see https://github.com/tree-sitter-perl/tree-sitter-perl
 ;; (setq treesit-language-source-alist
